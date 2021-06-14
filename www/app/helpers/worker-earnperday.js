e57792520cf390b787666c48159869060412a26e
Exlo <46317321+Exlo84@users.noreply.github.com>
2021-06-14 11:46:34 +0200

    Update worker-earnperday.js

--- www/app/helpers/worker-earnperday.js
+++ www/app/helpers/worker-earnperday.js
@@ -3,6 +3,6 @@ import config from '../config/environment';
 
 export default Ember.Helper.extend({
   compute(hashrates) {
-    return 24 * 60 * 60 * (hashrates[0] / hashrates[1]) * 0.8 * 0.95;
+    return 24 * 60 * 60 * (hashrates[0] / hashrates[1]) * 0.6 * 0.95;
   }
 });
