951b8231e823eeed4ab700427add9623cd17f5d7
Exlo <46317321+Exlo84@users.noreply.github.com>
2021-07-14 09:15:06 +0200

    Update frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -5,10 +5,12 @@
 #                     Modules and Running                  #
 # **********************************************************
 
+cd open-ethereum-pool
 cd www
 sudo chmod -R 777 . #Permission issues
-sudo npm install ember-cli@^2.18.2
-sudo npm install bower
+sudo npm install -g ember-cli@2.18.2
+sudo npm install ember-cli-cookie@0.2.0
+sudo npm install -g bower
 sudo chown -R $USER:$GROUP ~/.npm
 sudo chown -R $USER:$GROUP ~/.config
 sudo npm install
@@ -17,5 +19,6 @@ sudo npm i intl-format-cache
 
 sudo ./build.sh
 
-sudo ember server
+sudo chmod -R 755 . #Bring Permission Levels Down to Appropriate Level
 
+sudo ember server
